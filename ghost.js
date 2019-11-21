e04f70c18d25bacf393a1ebb3cb8463e86636b03
Tuple <45510984+Tuple-list@users.noreply.github.com>
2019-11-21 15:21:06 +0800

    Add files via upload

---
+++ ghost.js
@@ -0,0 +1,29 @@
+var count = 0;
+var isgo = false;
+var timer;
+
+window.onload = function () {
+    var ul_img = document.getElementsByClassName("ul_img")[0];
+    var li_img = document.getElementsByClassName("li_img");
+    showtime();
+    function showtime() {
+        timer = setInterval(function () {
+            if (isgo == false) {
+                count++;
+                ul_img.style.transform = "translate(" + -638 * count + "px)";
+                if (count >= li_img.length - 1) {
+                    count = li_img.length - 1;
+                    isgo = true;
+                }
+            }
+            else {
+                count--;
+                ul_img.style.transform = "translate(" + -638 * count + "px)";
+                if (count <= 0) {
+                    count = 0;
+                    isgo = false;
+                }
+            }
+        }, 3000)
+    }
+}
